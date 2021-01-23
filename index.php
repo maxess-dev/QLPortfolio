cc87d203bfb78e98ea8637b566ca7b7eb8f098e6
Maxime Schellenberger <maxess@Mac-mini-de-Maxime.local>
2021-01-23 01:16:58 +0100

    Home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!---- HEAD ---->
+<?php include('./vue/head.php') ?>
+<!---- BODY ---->
+<body>
+    <div id="slider">
+        <header>
+            <nav class="nav-ctn">
+                <ul>
+                    <li>explore</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+        </header>
+        <div class="slider-addon">
+            <div class="round-btn">
+                <span class="round"></span>
+                <span class="round"></span>
+                <span class="round"></span>
+                <span class="round"></span>
+            </div>
+        </div>
+        <footer>
+            <div class="name">
+                <span><h1>Quentin Leduff</h1></span>
+                <span><h2>3D Designer</h2></span>
+            </div>
+        </footer>
+    </div>
+</body>
+<!---- FOOTER ---->
+<?php include('./vue/footer.php') ?>
---
+++ vue/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quentin Leduff | CG Artist</title>
+    <!-- SCSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<!-- REMOVE IN PROD => sass --watch ./assets/scss/style.scss ./assets/css/style.css -->
