e26470e2710004c9f709781afde819cb6089546d
Maxime Schellenberger <maxime@Mac-mini-de-Maxime.local>
2021-02-03 14:43:55 +0100

    size menu

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
             <div class="list-icons">
                 <ul>
                     <li>
-                        <a href="https://www.youtube.com/c/QuentinLeDuff/videos" target="_blank"><img src="./assets/icons/youtube.svg" alt="Youtube link" class="icons" width="20" height="20"></a>
+                        <a href="https://www.youtube.com/c/QuentinLeDuff/videos" rel="noopener noreferrer" target="_blank"><img src="./assets/icons/youtube.svg" alt="Youtube link" class="icons" width="20" height="20"></a>
                     </li>
                     <li>
-                        <a href="https://www.behance.net/quentinleduff-3d?tracking_source=search_users_recommended%7Cquentin%20leduff" target="_blank"><img src="./assets/icons/behance.svg" alt="Behance link" class="icons" width="20" height="20"></a>
+                        <a href="https://www.behance.net/quentinleduff-3d?tracking_source=search_users_recommended%7Cquentin%20leduff" rel="noopener noreferrer" target="_blank"><img src="./assets/icons/behance.svg" alt="Behance link" class="icons" width="20" height="20"></a>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/profile.php?id=100009886263876" target="_blank"><img src="./assets/icons/facebook.svg" alt="Facebook link" class="icons" width="20" height="20"></a>
+                        <a href="https://www.facebook.com/profile.php?id=100009886263876" rel="noopener noreferrer" target="_blank"><img src="./assets/icons/facebook.svg" alt="Facebook link" class="icons" width="20" height="20"></a>
                     </li>
                     <li>
-                        <a href="https://twitter.com/Blender_Dragon" target="_blank"><img src="./assets/icons/twitter.svg" alt="Twitter link" class="icons" width="20" height="20"></a>
+                        <a href="https://twitter.com/Blender_Dragon" rel="noopener noreferrer" target="_blank"><img src="./assets/icons/twitter.svg" alt="Twitter link" class="icons" width="20" height="20"></a>
                     </li>
                 </ul>
             </div>
